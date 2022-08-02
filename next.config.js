6b27a69c363d3117ff7bf136136a1a01a223c3ea
Nabarun <nicknabarun@gmail.com>
2022-08-02 21:30:50 +0530

    Add env object

--- next.config.js
+++ next.config.js
@@ -6,4 +6,12 @@ module.exports = {
     // loader: "akamai",
     // path: "",
   },
+  env: {
+    NEXT_APP_FIREBASE_API_KEY: process.env.apiKey,
+    NEXT_APP_FIREBASE_AUTH_DOMAIN: process.env.authDomain,
+    NEXT_APP_FIREBASE_PROJECT_ID: process.env.projectId,
+    NEXT_APP_FIREBASE_STORAGE_BUCKET: process.env.storageBucket,
+    NEXT_APP_FIREBASE_MESSAGING_SENDER_ID: process.env.messagingSenderId,
+    NEXT_APP_FIREBASE_APP_ID: process.env.appId,
+  },
 };
